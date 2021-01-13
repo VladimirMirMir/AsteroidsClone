32b953a903e4fb930607419e617c8e4d58b28c7c
VladimirMirMir <75939896+VladimirMirMir@users.noreply.github.com>
2021-01-14 00:46:36 +0300

    Final Polish

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -22,40 +22,11 @@ public class AsteroidController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //..смотрим какого размера был астероид и спавним астероиды дочерние
-        switch (tag)
-        {
-            case "Asteroid":
-                break;
-            case "MediumAsteroid":
-                SpawnSmallAsteroids(collision);
-                break;
-            case "BigAsteroid":
-                SpawnSmallAsteroids(collision);
-                SpawnSmallAsteroids(collision);
-                break;
-            case "HugeAsteroid":
-                SpawnSmallAsteroids(collision);
-                SpawnSmallAsteroids(collision);
-                SpawnSmallAsteroids(collision);
-                break;
-        }
         //Если столкнулся с игроком...
         if (collision.tag == "Player")
         {
             //Спавн взрыва
             GameManager.SpawnExplosion(gameObject, collision.gameObject, 0);
         }
-    }
-
-    private void SpawnSmallAsteroids(Collider2D collision)
-    {
-        //Спавн маленького астероида в взрывающемся астероиде
-        GameObject local = GameManager.SpawnAsteroid(collision.transform);
-        //Задаём случайные скорость и направление
-        Rigidbody2D localRB = local.GetComponent<AsteroidController>().rb;
-        localRB = local.GetComponent<Rigidbody2D>();
-        localRB.velocity = new Vector2(Random.Range(-1f, 1f) , Random.Range(-1f, 1f)) * GameManager.Instance.AsteroidSpeed;
-        local.GetComponent<AsteroidController>().rb = localRB;
     }
 }
